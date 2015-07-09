86c5b9e8c48bdda145583f0717d6d2f6d12c15fd
RohitMobisys <rohitpanhalkar001@gmail.com>
2015-07-09 05:19:42 -0700

    Initial Commit

---
+++ DatabaseConcepts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatabaseConcepts
+//
+//  Created by Mobisys on 7/9/15.
+//  Copyright (c) 2015 Mobisys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DatabaseConcepts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DatabaseConcepts
+//
+//  Created by Mobisys on 7/9/15.
+//  Copyright (c) 2015 Mobisys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
